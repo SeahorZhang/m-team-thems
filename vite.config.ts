f52d6e91403afd9b9052a656c80b6cfece64407e
xihe <im0k@outlook.com>
2025-06-02 19:37:51 +0800

    refactor: 移除vite.config.ts中的cdn相关配置，并简化monkey插件导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import monkey, { cdn } from 'vite-plugin-monkey';
+import monkey from 'vite-plugin-monkey';
 import svgr from "vite-plugin-svgr";
 import tailwindcss from '@tailwindcss/vite'
 import { name, version, description, author, license } from "./package.json";
@@ -26,13 +26,13 @@ export default defineConfig({
         match: ['https://next.m-team.cc/*'],
         icon: 'https://next.m-team.cc/favicon.ico',
       },
-      build: {
-        externalGlobals: {
-          react: cdn.jsdelivr('React', 'umd/react.production.min.js'),
-          'react-dom': cdn.jsdelivr('ReactDOM', 'umd/react-dom.production.min.js'),
-          antd: cdn.jsdelivr('antd', 'antd.min.js')
-        },
-      },
+      // build: {
+      //   externalGlobals: {
+      //     react: cdn.jsdelivr('React', 'umd/react.production.min.js'),
+      //     'react-dom': cdn.jsdelivr('ReactDOM', 'umd/react-dom.production.min.js'),
+      //     antd: cdn.jsdelivr('antd', 'antd.min.js')
+      //   },
+      // },
     }),
   ],
 })
