8722dcd277aa818338f6286b15c39f02560b9ec1
xihe <im0k@outlook.com>
2025-06-08 15:19:40 +0800

    chore: 更新版本号至1.1.1，更改海洋和樱花图案，优化GitHub组件和CSS样式

--- src/components/Github.tsx
+++ src/components/Github.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Space, Tooltip } from "antd";
 import { GithubOutlined } from "@ant-design/icons";
+import SimpleIconsTampermonkey from "../assets/simpleIconsTampermonkey.svg?react";
+import Icon from "@ant-design/icons";
 import { version, license } from "../../package.json";
+const youhou = "https://greasyfork.org/zh-CN/scripts/538074-m-team-thems";
 
 declare const __BUILD_TIME__: string;
 
 const Github: React.FC = () => {
   return (
-    <Space
+    <div
       style={{
         padding: "4px 8px",
         display: "flex",
@@ -16,16 +19,39 @@ const Github: React.FC = () => {
         width: "100%",
       }}
     >
-      <Tooltip title="查看源码" placement="bottom">
-        <a
-          href={license}
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ color: "inherit" }}
-        >
-          <GithubOutlined style={{ fontSize: 16 }} />
-        </a>
-      </Tooltip>
+      <Space size="small" align="center">
+        <Tooltip title="查看源码" placement="bottom">
+          <a
+            href={license}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              color: "inherit",
+            }}
+            className="icon-link"
+          >
+            <GithubOutlined style={{ fontSize: 18 }} />
+          </a>
+        </Tooltip>
+        <Tooltip title="油猴" placement="bottom">
+          <a
+            href={youhou}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              color: "inherit",
+            }}
+            className="icon-link"
+          >
+            <Icon
+              style={{
+                fontSize: 18,
+              }}
+              component={SimpleIconsTampermonkey}
+            />
+          </a>
+        </Tooltip>
+      </Space>
 
       <Tooltip title={`更新时间：${__BUILD_TIME__}`} placement="bottom">
         <span
@@ -37,7 +63,7 @@ const Github: React.FC = () => {
           v{version}
         </span>
       </Tooltip>
-    </Space>
+    </div>
   );
 };
 
